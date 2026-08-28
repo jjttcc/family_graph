46df8406aad843ba021c1ee3fb6c1ba1473ea5a1
Jim Cochrane <jim.cochrane@gmail.com>
2026-08-28 12:22:44 -0400

    driver, main.rb, is now functional

--- src/main.rb
+++ src/main.rb
@@ -1,11 +1,27 @@
-# top level
+#!/usr/bin/env ruby
 
-# required libraries/tools
-require 'ruby_contracts'
+require_relative 'data_loader'
+require_relative 'graph'
+require_relative 'graph_renderer'
+require_relative 'family_constants'
 
-# application components
-require 'coordinates'
-require 'root_person'
+# Main driver to generate the family tree SVG
+data_path = '/home3/development/jtc/relatives/gemini/output-formatting/' \
+            'manual_engine/data/master_tree.yaml'
 
-root = root_person
-graph = Graph.new(root_person)
+puts "Loading data from #{data_path}..."
+people = DataLoader.load(data_path)
+
+# Using john_frost_1680 as the default root
+root_id = 'john_frost_1680'
+root = people[root_id]
+puts "Generating graph for #{root_id}..."
+graph = Graph.new(root)
+
+# Default direction is :ancestry
+puts "Rendering SVG..."
+renderer = GraphRenderer.new(graph.coordinates, people, :ancestry)
+output_dir = '/home3/development/jtc/relatives/gemini/output-formatting/family_graph/output'
+renderer.render(output_dir)
+
+puts "Successfully rendered graph to output/"
